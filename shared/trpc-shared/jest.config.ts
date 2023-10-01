c9e5eeabdecb56318def372c148131181a536321
j0sephh123 <30373032+j0sephh123@users.noreply.github.com>
2023-10-01 16:16:43 +0300

    move trpc-shared to shared folder (#6)

--- shared/trpc-shared/jest.config.ts
+++ shared/trpc-shared/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'trpc-shared',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/libs',
+  coverageDirectory: '../../coverage/shared/trpc-shared',
 };
