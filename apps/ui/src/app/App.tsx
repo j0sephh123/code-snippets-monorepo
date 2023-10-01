61803a1f6821d53500d36a0990ae879c5050427a
Joseph <iossif123gg@gmail.com>
2023-10-01 09:16:03 +0300

    generate ui app with Vite and React

---
+++ apps/ui/src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome ui 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
