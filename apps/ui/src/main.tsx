08a07ac952e553ffdcd1ace4613284cedeac2320
Joseph <iossif123gg@gmail.com>
2023-10-02 21:46:56 +0300

    add mantine library

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -3,12 +3,19 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import TRPCProvider from './app/components/TRPCProvider';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { createTheme } from '@mantine/core';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <TRPCProvider>
-        <App />
+        <MantineProvider defaultColorScheme="dark" theme={theme}>
+          <App />
+        </MantineProvider>
       </TRPCProvider>
     </BrowserRouter>
   </StrictMode>
