b62b51072cb690cb43da0f6289c71222b82d51bf
iossifbenbassat123 <iossif.benbassat@strypes.eu>
2023-10-10 22:45:53 +0300

    add test for ColorThemeSwitch

--- apps/ui/src/components/ColorThemeSwitch/ColorThemeSwitch.test.tsx
+++ apps/ui/src/components/ColorThemeSwitch/ColorThemeSwitch.test.tsx
@@ -1,30 +1,32 @@
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
-import { vi } from 'vitest';
+import ColorThemeSwitch from './ColorThemeSwitch';
 import { MantineProvider, createTheme } from '@mantine/core';
-import CodeSnippetDialogTrigger from './CodeSnippetDialogTrigger';
 
-const toggleDialogSpy = vi.hoisted(vi.fn);
+const setColorSchemeSpy = vi.hoisted(vi.fn);
 
-vi.mock('../../store/dialog/dialogState', () => ({
-  toggleDialog: toggleDialogSpy,
-}));
+vi.mock('@mantine/core', async () => {
+  const mantineActual = await vi.importActual('@mantine/core');
 
-describe('components > CodeSnippetDialogTrigger', () => {
+  return {
+    ...(mantineActual as any),
+    useMantineColorScheme: () => ({ setColorScheme: setColorSchemeSpy }),
+  };
+});
+
+describe('components > ColorThemeSwitch', () => {
   it('renders', async () => {
     const user = userEvent.setup();
     const { asFragment } = render(
       <MantineProvider defaultColorScheme="dark" theme={createTheme({})}>
-        <CodeSnippetDialogTrigger />
+        <ColorThemeSwitch />
       </MantineProvider>
     );
     const btn = screen.getByRole('button');
 
     await user.click(btn);
 
-    expect(toggleDialogSpy).toHaveBeenCalledWith('create', {
-      title: 'Create a Code Snippet',
-    });
+    expect(setColorSchemeSpy).toHaveBeenCalledWith('light');
     expect(asFragment()).toMatchSnapshot();
   });
 });
