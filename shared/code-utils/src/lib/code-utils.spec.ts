7ddad680f999a3ce55175bd321c4e6e4fb7d3889
Joseph <iossif123gg@gmail.com>
2023-10-14 09:49:06 +0300

    re-write identifyCodeType to couple with Language interface instead

--- shared/code-utils/src/lib/code-utils.spec.ts
+++ shared/code-utils/src/lib/code-utils.spec.ts
@@ -1,32 +1,32 @@
 import { identifyCodeType } from './code-utils';
 
 describe('identifyCodeType', () => {
-  test('should return "invalid" for an invalid code snippet', () => {
-    expect(identifyCodeType('const x = ')).toBe('invalid');
+  test('should return null for an invalid code snippet', () => {
+    expect(identifyCodeType('const x = ')).toBe(null);
   });
 
   // Test Cases for TypeScript Identification
-  test('should return "ts" for TypeScript snippet with type annotations', () => {
-    expect(identifyCodeType('const x: number = 10;')).toBe('ts');
+  test('should return "TypeScript" for TypeScript snippet with type annotations', () => {
+    expect(identifyCodeType('const x: number = 10;')).toBe('TypeScript');
   });
 
-  test('should return "ts" for TypeScript snippet with interfaces', () => {
+  test('should return "TypeScript" for TypeScript snippet with interfaces', () => {
     expect(identifyCodeType('interface Point { x: number, y: number }')).toBe(
-      'ts'
+      'TypeScript'
     );
   });
 
   // Test Cases for JavaScript Identification
-  test('should return "js" for JavaScript snippet using var', () => {
-    expect(identifyCodeType('var x = 10;')).toBe('js');
+  test('should return "JavaScript" for JavaScript snippet using var', () => {
+    expect(identifyCodeType('var x = 10;')).toBe('JavaScript');
   });
 
-  test('should return "js" for JavaScript snippet using let and const', () => {
-    expect(identifyCodeType('let x = 10; const y = 20;')).toBe('js');
+  test('should return "JavaScript" for JavaScript snippet using let and const', () => {
+    expect(identifyCodeType('let x = 10; const y = 20;')).toBe('JavaScript');
   });
 
   // Combined Test Cases
-  test('should return "js" for snippet valid in both TypeScript and JavaScript', () => {
-    expect(identifyCodeType('const x = 10;')).toBe('js');
+  test('should return "JavaScript" for snippet valid in both TypeScript and JavaScript', () => {
+    expect(identifyCodeType('const x = 10;')).toBe('JavaScript');
   });
 });
--- shared/code-utils/src/lib/code-utils.ts
+++ shared/code-utils/src/lib/code-utils.ts
@@ -1,19 +1,23 @@
 import * as parser from '@babel/parser';
+import { Language } from '@prisma/client';
 
-// This is ugly, but with the current implementation 
+// This is ugly, but with the current implementation
 // plugins: ['typescript'] can also parse JS code
-// to avoid checking for explicit TS constructs, these implementation 
+// to avoid checking for explicit TS constructs, these implementation
 // seems to satisfy the necessary business logic represented as test cases
-export function identifyCodeType(codeSnippet: string): 'invalid' | 'ts' | 'js' {
+
+export function identifyCodeType(codeSnippet: string): Language | null {
   let isJS = true;
   let isTS = true;
 
+  // Check if code is JavaScript
   try {
     parser.parse(codeSnippet, { sourceType: 'module' });
   } catch (e) {
     isJS = false;
   }
 
+  // Check if code is TypeScript
   try {
     parser.parse(codeSnippet, {
       sourceType: 'module',
@@ -23,19 +27,20 @@ export function identifyCodeType(codeSnippet: string): 'invalid' | 'ts' | 'js' {
     isTS = false;
   }
 
+  // Check and return the detected language or null if invalid
   if (!isJS && !isTS) {
-    return 'invalid';
+    return null;
   }
 
   if (isTS && !isJS) {
-    return 'ts';
+    return 'TypeScript';
   }
 
   if (isJS) {
-    return 'js';
+    return 'JavaScript';
   }
 
   // This should never be reached based on current logic,
   // but it's here to satisfy TypeScript's requirement that all code paths must return a value.
-  return 'invalid';
+  return null;
 }
